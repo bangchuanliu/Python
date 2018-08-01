c1379cfc02afdace936960b7bddb4c7009bb67d0
bangchuanliu <bangchuanliu@bangchuanlius-MacBook-Pro.local>
2018-08-01 03:00:18 -0700

    urllib2 example

--- url.py
+++ url.py
@@ -1,5 +1,14 @@
+import urllib
 import urllib2
 
-res = urllib2.urlopen("https://www.google.com")
-print res.info()
-#print res.read()
+url = 'http://www.someserver.com/cgi-bin/register.cgi'
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+values = {'name': 'Michael Foord',
+          'location': 'Northampton',
+          'language': 'Python' }
+headers = {'User-Agent': user_agent}
+
+data = urllib.urlencode(values)
+req = urllib2.Request(url, data, headers)
+response = urllib2.urlopen(req)
+the_page = response.read()
